2883983977b7fbd28d3fd5300a1e475e122114c8
Jordan Hoover <jordan@hoovermld.com>
2019-06-23 22:22:29 -0500

    Add activity panel functionality.

--- js/script.js
+++ js/script.js
@@ -27,81 +27,137 @@ const settShirtInfo = () => {
   const jsPunOptions = ["cornflowerblue", "darkslategrey", "gold"];
   const iHeartJsOptions = ["tomato", "steelblue", "dimgrey"];
 
-  $("#design").change(() => {
+  const $designSelect = $("#design");
+
+  $designSelect.change(() => {
+    let filterArr = [];
+    if ($designSelect.val() === "js puns") {
+      filterArr = jsPunOptions;
+    } else if ($designSelect.val() === "heart js") {
+      filterArr = iHeartJsOptions;
+    }
+
     for (let colorChoice of tShirtColorSelect.children()) {
       colorChoice = $(colorChoice);
+      colorChoice.show();
 
-      if (jsPunOptions.includes(colorChoice.val())) {
-        colorChoice.show();
-      } else {
+      if (filterArr.length !== 0 && !filterArr.includes(colorChoice.val())) {
         colorChoice.hide();
       }
     }
   });
 };
 
-const setRegisterForActivities = () => {
-  const activities = $(".activities label");
+const activityRegisterPanel = (function($) {
+  const activityPanel = $(".activities");
+  const totalEl = $("<p></p>");
 
-  activities.children(":checkbox").click(e => {
-    const selectedCheckbox = $(e.target);
-    const selectedLabel = selectedCheckbox.parent();
-    const selectedLabelText = selectedLabel.text();
-
-    const currentBoxTime = getActivityValues(selectedLabelText).time;
-
-    let checkedLabels = selectedLabel.siblings().has("input");
-    for (let checkedLabel of checkedLabels) {
-      const labelText = checkedLabel.textContent;
-      const checkBox = $(checkedLabel).children();
-
-      const activityTime = getActivityValues(labelText).time;
-
-      if (activityTime === currentBoxTime && selectedCheckbox.is(":checked")) {
-        checkBox.attr("disabled", true);
-        checkBox.attr("checked", false);
-      } else {
-        checkBox.attr("disabled", false);
-      }
-    }
-  });
-};
-
-const getActivityValues = activityText => {
-  const activityVals = {
-    name: "",
-    time: "",
-    cost: 0
+  const setActivityPanel = () => {
+    appendWorkshopTotalEl(activityPanel);
+    setActivityCheckEvent();
   };
 
-  let activitySplit = activityText.split(" — ");
+  const setActivityCheckEvent = () => {
+    const activities = activityPanel.find("label");
 
-  activityVals.name = activitySplit[0].trim();
+    activities.children(":checkbox").click(e => {
+      setTotalVal(calculateCost(activities));
+      hideOverlappingActivities(activities);
+    });
+  };
 
-  let nextSplit = activitySplit[1].split(",");
+  const hideOverlappingActivities = activities => {
+    const checkedActivities = activities.filter(":has(:checked)");
+    const unCheckedActivities = activities.filter(":not(:has(:checked))");
 
-  let rawCost;
-  if (nextSplit.length > 1) {
-    activityVals.time = nextSplit[0];
-    rawCost = nextSplit[1].trim();
-  } else {
-    activityVals.time = "";
-    rawCost = nextSplit[0];
-  }
+    let checkedActivityTimes = [];
+    for (let checkedActivity of checkedActivities) {
+      const checkedActivityTime = getActivityValues(checkedActivity.textContent)
+        .time;
+      checkedActivityTimes.push(checkedActivityTime);
+    }
 
-  activityVals.cost = Number(rawCost.replace(/[^0-9.]+/g, ""));
+    for (let activity of unCheckedActivities) {
+      activity = $(activity);
 
-  return activityVals;
-};
+      const activityTime = getActivityValues(activity.text()).time;
+      const activityCheckbox = activity.children();
 
-const calcWorkshopCost = () => {
-  // TODO: Tally running cost of workshops.
-};
+      if (checkedActivityTimes.includes(activityTime)) {
+        activityCheckbox.attr("disabled", true);
+        activityCheckbox.attr("checked", false);
+      } else {
+        activityCheckbox.attr("disabled", false);
+      }
+    }
+  };
+
+  const calculateCost = activities => {
+    let totalAmt = 0;
+
+    for (let activity of activities) {
+      activity = $(activity);
+
+      const labelText = activity.text();
+      const checkBox = activity.children();
+
+      const activityValues = getActivityValues(labelText);
+
+      if (checkBox.is(":checked")) {
+        totalAmt += activityValues.cost;
+      }
+    }
+
+    return totalAmt;
+  };
+
+  const getActivityValues = activityText => {
+    const activityVals = {
+      name: "",
+      time: "",
+      cost: 0
+    };
+
+    let activitySplit = activityText.split(" — ");
+
+    activityVals.name = activitySplit[0].trim();
+
+    let nextSplit = activitySplit[1].split(",");
+
+    let rawCost;
+    if (nextSplit.length > 1) {
+      activityVals.time = nextSplit[0];
+      rawCost = nextSplit[1].trim();
+    } else {
+      activityVals.time = "";
+      rawCost = nextSplit[0];
+    }
+
+    activityVals.cost = Number(rawCost.replace(/[^0-9.]+/g, ""));
+
+    return activityVals;
+  };
+
+  const setTotalVal = totalAmt => {
+    totalEl.text(`Total Cost: $${totalAmt}`);
+  };
+
+  const appendWorkshopTotalEl = parentEl => {
+    setTotalVal(0);
+
+    parentEl.append(totalEl);
+    return totalEl;
+  };
+
+  return {
+    setActivityPanel
+  };
+})(jQuery);
 
 const run = () => {
   focusFirstField();
   showOtherJobRoleField();
   settShirtInfo();
-  setRegisterForActivities();
+  activityRegisterPanel.setActivityPanel();
 };
 run();
